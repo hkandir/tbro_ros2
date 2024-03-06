274d197c26d9300933a1c8cf1de0455bd39169a5
Huseyin Kandir <huseyin.kandir@colorado.edu>
2024-03-05 22:02:59 -0700

    tf_transformations is added

--- tbro/tbro_node.py
+++ tbro/tbro_node.py
@@ -5,6 +5,9 @@ Transformer Based Radar Odometry ROS node
 # generic
 import rclpy
 from rclpy.node import Node
+import math
+import numpy as np
+from tf_transformations import quaternion_from_euler
 
 # replace this with dca1000_device/msg/MimoMsg
 # ros messages
@@ -30,10 +33,11 @@ class TbroSubscriber(Node):
 
     def __init__(self):
         super().__init__("tbro_subscriber")
-        self.controller_period = 0.1
+        self.controller_period = 0.01
         self.init_flag = False  # False if not initialzied
         self.msg_buffer = []
         self.data_set = MimoDataset()
+        self.tbro_odometrty = np.zeros(6)
         self.args = Parameters()
         self.device = torch.device("cpu")
         # self.model = DeepROEncOnly(self.args)
@@ -85,17 +89,29 @@ class TbroSubscriber(Node):
                 [self.data_set.__getitem__(0)[0], self.data_set.__getitem__(1)[0]]
             )
 
+        self.tbro_odometrty[0] += float(odom[0][0])
+        self.tbro_odometrty[1] += float(odom[0][1])
+        self.tbro_odometrty[2] += float(odom[0][2])
+        self.tbro_odometrty[3] += float(odom[0][3])
+        self.tbro_odometrty[4] += float(odom[0][4])
+        self.tbro_odometrty[5] += float(odom[0][5])
+
         odom_msg = Odometry()
         odom_msg.header.stamp = self.get_clock().now().to_msg()
         odom_msg.header.frame_id = "world"
         odom_msg.child_frame_id = "imu_link_enu"
-        odom_msg.pose.pose.position.x = float(odom[0][0])
-        odom_msg.pose.pose.position.y = float(odom[0][1])
-        odom_msg.pose.pose.position.z = float(odom[0][2])
-        odom_msg.pose.pose.orientation.x = float(odom[0][3])
-        odom_msg.pose.pose.orientation.y = float(odom[0][4])
-        odom_msg.pose.pose.orientation.z = float(odom[0][5])
-        odom_msg.pose.pose.orientation.w = float(1.0)
+        odom_msg.pose.pose.position.x = self.tbro_odometrty[0]
+        odom_msg.pose.pose.position.y = self.tbro_odometrty[1]
+        odom_msg.pose.pose.position.z = self.tbro_odometrty[2]
+
+        quaternion_frame = quaternion_from_euler(
+            float(odom[0][3]), float(odom[0][4]), float(odom[0][5])
+        )
+
+        odom_msg.pose.pose.orientation.x = quaternion_frame[0]
+        odom_msg.pose.pose.orientation.y = quaternion_frame[1]
+        odom_msg.pose.pose.orientation.z = quaternion_frame[2]
+        odom_msg.pose.pose.orientation.w = quaternion_frame[3]
 
         print("Publishing pose: ", odom_msg.pose.pose)
 
